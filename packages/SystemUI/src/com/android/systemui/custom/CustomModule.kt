19aedda85b91828d930914b88d1b0a3d3a6dbada
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I0e3cff73752c59d804dcec43b86524c2e8df4f57

--- packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
+++ packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
@@ -21,6 +21,8 @@ import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.WifiTile
+import com.android.systemui.qs.tiles.HeadsUpTile
+
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -51,4 +53,10 @@ interface CustomModule {
     @IntoMap
     @StringKey(WifiTile.TILE_SPEC)
     fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
+
+    /** Inject HeadsUpTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(HeadsUpTile.TILE_SPEC)
+    fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
 }
