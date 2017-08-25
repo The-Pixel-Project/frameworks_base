ce9c21345d93d85adff56e306b8e57316f15ba4d
Alexander Martinz <alex@amartinz.at>
2017-08-25 12:01:11 +0200

    BatteryService: Add support for oem fast charger detection
    Based on AOSPA fw/base 0af31949c69f7c306b49ecf2c25579300973fdc9
      - https://github.com/AOSPA/android_frameworks_base/commit/0af31949c69f7c306b49ecf2c25579300973fdc9
    
    Allows to indicate whether a device is charged using a
    proprietary oem fast charge solution which writes status
    to sysfs.
    [nift4: Some devices turn off fast charging when display is on,
    which is why even though AOSP detects the fast charger, the user won't
    actually get to see it without this commit.]
    
    The oem fast charge detection tries to be as generic as possible and is
    configured via overlays.
    
    nift4:
    - port to 13
    - support using custom value for path, don't hardcode 1
    - big cleanup
    
    Change-Id: Ia91a9c3bb20967c5aac18c24ac7c2be10a0b8bf1
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -300,6 +300,9 @@ public final class BatteryService extends SystemService {
 
     private boolean mBatteryLevelLow;
 
+    private boolean mOemFastCharging;
+    private boolean mLastOemFastCharging;
+
     private long mDischargeStartTime;
     private int mDischargeStartLevel;
 
@@ -758,6 +761,7 @@ public final class BatteryService extends SystemService {
 
         shutdownIfNoPowerLocked();
         shutdownIfOverTempLocked();
+        mOemFastCharging = isOemFastCharging();
 
         if (force || mHealthInfo.chargingPolicy != mLastChargingPolicy) {
             mLastChargingPolicy = mHealthInfo.chargingPolicy;
@@ -782,7 +786,8 @@ public final class BatteryService extends SystemService {
                 || mInvalidCharger != mLastBroadcastInvalidCharger
                 || mHealthInfo.batteryCycleCount != mLastBroadcastBatteryCycleCount
                 || mHealthInfo.chargingState != mLastBroadcastChargingState
-                || mHealthInfo.batteryCapacityLevel != mLastBroadcastBatteryCapacityLevel)) {
+                || mHealthInfo.batteryCapacityLevel != mLastBroadcastBatteryCapacityLevel
+                || mOemFastCharging != mLastOemFastCharging)) {
 
             if (mPlugType != mLastBroadcastPlugType) {
                 if (mLastBroadcastPlugType == BATTERY_PLUGGED_NONE) {
@@ -1014,6 +1019,7 @@ public final class BatteryService extends SystemService {
                 mLastBroadcastBatteryCapacityLevel = mHealthInfo.batteryCapacityLevel;
                 mLastBroadcastBatteryFullCharge = mHealthInfo.batteryFullChargeUah;
                 mLastBroadcastBatteryFullChargeDesign = mHealthInfo.batteryFullChargeDesignCapacityUah;
+                mLastOemFastCharging = mOemFastCharging;
             }
         }
     }
@@ -1053,9 +1059,11 @@ public final class BatteryService extends SystemService {
         intent.putExtra(
                 BatteryManager.EXTRA_DESIGN_CAPACITY,
                 mHealthInfo.batteryFullChargeDesignCapacityUah);
+        intent.putExtra(BatteryManager.EXTRA_OEM_FAST_CHARGING, mOemFastCharging);
         if (DEBUG) {
             Slog.d(TAG, "Sending ACTION_BATTERY_CHANGED. scale:" + BATTERY_SCALE
-                    + ", info:" + mHealthInfo.toString());
+                    + ", info:" + mHealthInfo.toString()
+                    + ", mOemFastCharging:" + mOemFastCharging);
         }
 
         if (com.android.server.flags.Flags.consolidateBatteryChangeEvents()) {
@@ -1200,6 +1208,25 @@ public final class BatteryService extends SystemService {
                 : mChargingPolicyChangeListeners) {
             listener.onChargingPolicyChanged(newPolicy);
         }
+    }
+
+    private boolean isOemFastCharging() {
+        for (String val : mContext.getResources().getStringArray(
+                com.android.internal.R.array.config_oemFastChargerStatusPaths)) {
+            String[] splitVal = val.split(";", 2);
+            String path = splitVal[0];
+            String expectedValue = splitVal[1];
+            try {
+                String value = FileUtils.readTextFile(new File(path), 0, null);
+                if (expectedValue.equals(value.trim())) {
+                    return true;
+                }
+            } catch (IOException e) {
+                Slog.e(TAG, "Failed to read oem fast charger status path: "
+                    + path, e);
+            }
+        }
+        return false;
     }
 
     // TODO: Current code doesn't work since "--unplugged" flag in BSS was purposefully removed.
