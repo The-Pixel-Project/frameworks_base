24b34e4e06ba66a652867c02403e4c5ca90837a1
Ido Ben-Hur <idoybh2@gmail.com>
2021-05-13 18:48:20 +0300

    Screenrecord: Add an option to record for longer
    Unlimited time-wise. Only limit file size to 15GiB
    
    Change-Id: Ia16045dde478f65d1bee4a5167684c403f018906

--- packages/SystemUI/multivalentTests/src/com/android/systemui/screenrecord/RecordingServiceTest.java
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/screenrecord/RecordingServiceTest.java
@@ -147,7 +147,8 @@ public class RecordingServiceTest extends SysuiTestCase {
 
     @Test
     public void testLogStartFullScreenRecording() {
-        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, null, false);
+        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, null, false,
+                false);
         mRecordingService.onStartCommand(startIntent, 0, 0);
 
         verify(mUiEventLogger, times(1)).log(Events.ScreenRecordEvent.SCREEN_RECORD_START);
@@ -157,7 +158,8 @@ public class RecordingServiceTest extends SysuiTestCase {
     public void testLogStartPartialRecording() {
         MediaProjectionCaptureTarget target =
                 new MediaProjectionCaptureTarget(new LaunchCookie(), 12345);
-        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, target, false);
+        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, target, false,
+                false);
         mRecordingService.onStartCommand(startIntent, 0, 0);
 
         verify(mUiEventLogger, times(1)).log(Events.ScreenRecordEvent.SCREEN_RECORD_START);
@@ -190,7 +192,8 @@ public class RecordingServiceTest extends SysuiTestCase {
         // When the screen recording does not start properly
         doThrow(new RuntimeException("fail")).when(mScreenMediaRecorder).start();
 
-        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, null, false);
+        Intent startIntent = RecordingService.getStartIntent(mContext, 0, 0, false, null, false,
+                false);
         mRecordingService.onStartCommand(startIntent, 0, 0);
 
         assertUpdateState(false);
