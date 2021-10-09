0b655f4e3e7be4dfc99007e6bbce86be787f34c3
LuK1337 <priv.luk@gmail.com>
2021-10-09 21:28:49 +0200

    Fallback to global actions if assistant is not available
    Change-Id: I8c5c34f8b29967eace022ab3eed035b1e68efcb6

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -735,6 +735,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private int mCurrentUserId;
 
+    private AssistUtils mAssistUtils;
+
     // Maps global key codes to the components that will handle them.
     private GlobalKeyManager mGlobalKeyManager;
 
@@ -1638,6 +1640,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
+    private boolean hasAssistant() {
+        return mAssistUtils.getAssistComponentForUser(mCurrentUserId) != null;
+    }
+
     private int getResolvedLongPressOnPowerBehavior() {
         if (FactoryTest.isLongPressOnPowerOffEnabled()) {
             return LONG_PRESS_POWER_SHUT_OFF_NO_CONFIRM;
@@ -1645,7 +1651,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         // If the config indicates the assistant behavior but the device isn't yet provisioned, show
         // global actions instead.
-        if (mLongPressOnPowerBehavior == LONG_PRESS_POWER_ASSISTANT && !isDeviceProvisioned()) {
+        if (mLongPressOnPowerBehavior == LONG_PRESS_POWER_ASSISTANT &&
+                (!isDeviceProvisioned() || !hasAssistant())) {
             return LONG_PRESS_POWER_GLOBAL_ACTIONS;
         }
 
@@ -2560,6 +2567,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mContext.registerReceiver(mMultiuserReceiver, filter);
 
         mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+
+        mAssistUtils = new AssistUtils(mContext);
 
         mGlobalKeyManager = new GlobalKeyManager(mContext);
 
