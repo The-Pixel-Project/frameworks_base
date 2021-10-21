f3e0bc306f881727bc5158c80486899c40fa2190
ezio84 <brabus84@gmail.com>
2021-10-21 18:07:40 +0000

    base: Long press power button to toggle flashlight [1/2]
    * Thanks to beanstown106 for the initial longpress action calls in
       PhoneWindowManager (improved by lineage guys)
    
     * Remove the settings pref if the device doesn't have flashlight
    
     * Vibrate when toggling torch to let the user know of unwanted toggling
       in the pocket (even if vibration is disabled by user)
    
     * Allow torch action also on ambient display
    
    [ezio84] :
    - Fix after Android 12L merge
    
    [AEX Edits] :
    - Port to S
    - simplify code
    
    [mydongistiny]:
    - Fixed for android10
    
    [DennySPB] :
    - base: ScreenOff torch: Allow to disable torch when screen is on
    
    [mickaelmendes50] :
    - Adapt to AOSPA
    - Adapt to Android 13
    
     * We don't allow users to use the Double Press power option,
       so remove the non used code.
     * Removed "double press power to toggle torch"
     * Removed "handle camera gesture here instead of extensions"
     * Removed "Add 170ms delay for double tap action"
    
    [aswin7469] :
    - Adapt to 14 QPR3
    
    Change-Id: I12da044f86c7b625872607529cf8524615cf576b
    
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Co-Authored-By: DennySPb <dennyspb@gmail.com>
    Co-Authored-By: Mesquita <mesquita@aospa.co>

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -183,6 +183,8 @@ public class CommandQueue extends IStatusBar.Stub implements
     private static final int MSG_ENTER_DESKTOP = 80 << MSG_SHIFT;
     private static final int MSG_SET_SPLITSCREEN_FOCUS = 81 << MSG_SHIFT;
     private static final int MSG_TOGGLE_QUICK_SETTINGS_PANEL = 82 << MSG_SHIFT;
+    private static final int MSG_TOGGLE_CAMERA_FLASH = 83 << MSG_SHIFT;
+
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
     public static final int FLAG_EXCLUDE_RECENTS_PANEL = 1 << 1;
@@ -574,6 +576,11 @@ public class CommandQueue extends IStatusBar.Stub implements
          * @see IStatusBar#moveFocusedTaskToDesktop(int)
          */
         default void moveFocusedTaskToDesktop(int displayId) {}
+
+        /**
+         * @see IStatusBar#toggleCameraFlash
+         */
+        default void toggleCameraFlash() {}
     }
 
     @VisibleForTesting
@@ -1492,6 +1499,14 @@ public class CommandQueue extends IStatusBar.Stub implements
         mHandler.obtainMessage(MSG_ENTER_DESKTOP, args).sendToTarget();
     }
 
+    @Override
+    public void toggleCameraFlash() {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_TOGGLE_CAMERA_FLASH);
+            mHandler.sendEmptyMessage(MSG_TOGGLE_CAMERA_FLASH);
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -2003,6 +2018,11 @@ public class CommandQueue extends IStatusBar.Stub implements
                     boolean isImmersiveMode = args.argi2 != 0;
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).immersiveModeChanged(rootDisplayAreaId, isImmersiveMode);
+		    }
+		    break;
+                case MSG_TOGGLE_CAMERA_FLASH:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).toggleCameraFlash();
                     }
                     break;
                 case MSG_ENTER_DESKTOP: {
