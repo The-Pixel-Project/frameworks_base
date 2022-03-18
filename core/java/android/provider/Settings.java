008e6927cf7ba9ddf1c9aea6b2fa0b55d300a10b
LibXZR <i@xzr.moe>
2022-03-18 10:12:37 +0800

    base: Add support for window ignore secure [1/2]
    Feature: yt/KSCOPE-23
    Change-Id: Ia9447ca17f6b24152b64526c25710a77e1388ce6
    Signed-off-by: LibXZR <i@xzr.moe>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12949,6 +12949,13 @@ public final class Settings {
         public static final String DOZE_SINGLE_TAP_GESTURE_AMBIENT = "doze_single_tap_gesture_ambient";
 
         /**
+         * Control whether FLAG_SECURE is ignored for all windows.
+         * @hide
+         */
+        @Readable
+        public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
