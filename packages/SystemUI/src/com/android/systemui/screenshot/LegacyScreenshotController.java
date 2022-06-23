cb92f5ff85c117fe14ba9b2a15eb95018aebcae6
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-24 02:38:49 +0300

    SystemUI: screenshot: open long screenshot activity for partial screenshots
    The partial screenshot UI is basically unusable. Replace it
    with the long screenshot UI which offers cropping, saving,
    canceling, and sharing capabilities.
    
    Change-Id: I27f7a811838f7e3f2057f567c7a350255996a8cb

--- packages/SystemUI/src/com/android/systemui/screenshot/LegacyScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/LegacyScreenshotController.java
@@ -70,6 +70,7 @@ import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.flags.FeatureFlags;
 import com.android.systemui.res.R;
 import com.android.systemui.screenshot.TakeScreenshotService.RequestCallback;
+import com.android.systemui.screenshot.scroll.ScrollCaptureController;
 import com.android.systemui.screenshot.scroll.ScrollCaptureExecutor;
 import com.android.systemui.util.Assert;
 
@@ -257,6 +258,12 @@ public class LegacyScreenshotController implements InteractiveScreenshotHandler 
         Assert.isMainThread();
 
         mCurrentRequestCallback = requestCallback;
+
+        if (screenshot.getType() == WindowManager.TAKE_SCREENSHOT_SELECTED_REGION) {
+            startPartialScreenshotActivity(Process.myUserHandle());
+            finisher.accept(null);
+            return;
+        }
 
         if (screenshot.getBitmap() == null) {
             Log.e(TAG, "handleScreenshot: Screenshot bitmap was null");
@@ -488,6 +495,23 @@ public class LegacyScreenshotController implements InteractiveScreenshotHandler 
                     return Unit.INSTANCE;
                 }
         );
+    }
+
+    private void startPartialScreenshotActivity(UserHandle owner) {
+        Bitmap newScreenshot = mImageCapture.captureDisplay(mDisplay.getDisplayId(),
+                getFullScreenRect());
+        ScrollCaptureController.BitmapScreenshot bitmapScreenshot =
+                new ScrollCaptureController.BitmapScreenshot(mContext, newScreenshot);
+
+        mScrollCaptureExecutor.executeBatchScrollCapture(bitmapScreenshot,
+                () -> {
+                    final Intent intent = ActionIntentCreator.INSTANCE.createLongScreenshotIntent(
+                            owner, mContext);
+                    mContext.startActivity(intent);
+                },
+                (destination, onTransitionEnd, longScreenshot) -> {
+                    onTransitionEnd.run();
+                });
     }
 
     private void onScrollButtonClicked(UserHandle owner, ScrollCaptureResponse response) {
