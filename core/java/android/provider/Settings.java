e7ec51f463d98a54698fe5a402baa913d8c94908
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-10 01:20:15 +0530

    SystemUI: Make advanced reboot optional
    Allow toggling it, and disable it by default.
    
    Change-Id: If10fbf5dc591858255e399108758a9e5b4ca9cfb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6325,6 +6325,12 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Whether to show advanced reboot options in power menu
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
