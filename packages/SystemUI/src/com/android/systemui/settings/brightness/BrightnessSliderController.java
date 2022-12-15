7f58421ce4d40f146c730d19b97eddc0f3a1a6cf
Andrew Fluck <andrew@aospa.co>
2022-12-15 16:08:56 +0100

    SystemUI: Animate auto brightness toggle
    Also enforce restriction for toggle and fix the visibility of the
    toggle if the device does not support auto brightness
    
    Author: Andrew Fluck <andrew@aospa.co>
    Signed-off-by: Fazil Sheik <fazil.sheik96@gmail.com>
    Change-Id: Ia80713220e47948a3cdf244f7bf8f69ce4421c08

--- packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessSliderController.java
+++ packages/SystemUI/src/com/android/systemui/settings/brightness/BrightnessSliderController.java
@@ -23,7 +23,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
@@ -67,7 +67,6 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
     private boolean mTracking;
     private final FalsingManager mFalsingManager;
     private final UiEventLogger mUiEventLogger;
-    private ImageView mIconView;
 
     private final HapticSliderPlugin mBrightnessSliderHapticPlugin;
     private final ActivityStarter mActivityStarter;
@@ -93,14 +92,12 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
 
     BrightnessSliderController(
             BrightnessSliderView brightnessSliderView,
-            ImageView icon,
             FalsingManager falsingManager,
             UiEventLogger uiEventLogger,
             HapticSliderPlugin brightnessSliderHapticPlugin,
             ActivityStarter activityStarter,
             BrightnessWarningToast brightnessWarningToast) {
         super(brightnessSliderView);
-        mIconView = icon;
         mFalsingManager = falsingManager;
         mUiEventLogger = uiEventLogger;
         mBrightnessSliderHapticPlugin = brightnessSliderHapticPlugin;
@@ -115,13 +112,13 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
         return mView;
     }
 
-    public ImageView getIconView() {
-        return mIconView;
-    }
 
     @Override
     protected void onViewAttached() {
         mView.setOnSeekBarChangeListener(mSeekListener);
+        if (!mView.setOnCheckedChangeListener(mToggleChangeListener)) {
+            mToggleChangeListener = null;
+        }
         mView.setOnInterceptListener(mOnInterceptListener);
         if (mMirror != null) {
             mView.setOnDispatchTouchEventListener(this::mirrorTouchEvent);
@@ -131,6 +128,7 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
     @Override
     protected void onViewDetached() {
         mView.setOnSeekBarChangeListener(null);
+        mView.setOnCheckedChangeListener(null);
         mView.setOnDispatchTouchEventListener(null);
         mView.setOnInterceptListener(null);
     }
@@ -173,6 +171,7 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
         if (mMirror != null) {
             mMirror.setMax(mView.getMax());
             mMirror.setValue(mView.getValue());
+            mMirror.setToggleValue(mView.getToggleValue());
             mView.setOnDispatchTouchEventListener(this::mirrorTouchEvent);
         } else {
             // If there's no mirror, we may be the ones dispatching, events but we should not mirror
@@ -225,6 +224,19 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
     @Override
     public int getValue() {
         return mView.getValue();
+    }
+
+    @Override
+    public void setToggleValue(boolean value) {
+        mView.setToggleValue(value);
+        if (mMirror != null) {
+            mMirror.setToggleValue(value);
+        }
+    }
+
+    @Override
+    public boolean getToggleValue() {
+        return mView.getToggleValue();
     }
 
     @Override
@@ -296,6 +308,13 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
         }
     };
 
+    private CompoundButton.OnCheckedChangeListener mToggleChangeListener =
+            (buttonView, isChecked) -> {
+                if (mListener != null) {
+                    mListener.onCheckedChanged(isChecked);
+                }
+            };
+
     /**
      * Creates a {@link BrightnessSliderController} with its associated view.
      */
@@ -339,7 +358,11 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
                 Context context,
                 @Nullable ViewGroup viewRoot) {
             int layout = getLayout();
-            BrightnessSliderView root = (BrightnessSliderView) LayoutInflater.from(context)
+            boolean hasAutoBrightness = context.getResources().getBoolean(
+                    com.android.internal.R.bool.config_automatic_brightness_available);
+            LayoutInflater inflater = LayoutInflater.from(context);
+
+            BrightnessSliderView root = (BrightnessSliderView) inflater
                     .inflate(layout, viewRoot, false);
             HapticSliderPlugin plugin = new HapticSliderPlugin(
                     mVibratorHelper,
@@ -347,8 +370,10 @@ public class BrightnessSliderController extends ViewController<BrightnessSliderV
                     mSystemClock,
                     new HapticSlider.SeekBar(root.requireViewById(R.id.slider)));
             HapticSliderViewBinder.bind(viewRoot, plugin);
-            ImageView icon = (ImageView) root.findViewById(R.id.brightness_icon);
-            return new BrightnessSliderController(root, icon, mFalsingManager, mUiEventLogger, plugin,
+            if (hasAutoBrightness) {
+                inflater.inflate(R.layout.quick_settings_auto_brightness, root, true);
+            }
+            return new BrightnessSliderController(root, mFalsingManager, mUiEventLogger, plugin,
                     mActivityStarter, mBrightnessWarningToast);
         }
 
