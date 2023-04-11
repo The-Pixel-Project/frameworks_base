ae6907b5238725dac7f3341e1214719c4c37da61
ezio84 <brabus84@gmail.com>
2023-04-11 20:20:59 +0300

    base: Allow showing Ambient instead of fully waking [1/2]
    Hey Google, please kang only good features by iOS -_-'
    
    Ido Ben-Hur (YAAP):
    Separate for dt2w and lift to wake
    Better code
    
    [AOSPA: Kill Double tap to ambient display]
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: Ib831888aadc601519a4a5f0b6576d44a337435b3
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12396,6 +12396,19 @@ public final class Settings {
         public static final String FEATURE_TOUCH_HOVERING = "feature_touch_hovering";
 
         /**
+         * Whether to show ambient instead of waking for the tap gesture
+         * @hide
+         */
+        public static final String DOZE_TAP_GESTURE_AMBIENT = "doze_tap_gesture_ambient";
+
+        /**
+         * Whether to show ambient instead of waking for the pickup gesture
+         * Do note quick pickup (device sensor) is already configured to do that
+         * @hide
+         */
+        public static final String DOZE_PICK_UP_GESTURE_AMBIENT = "doze_pick_up_gesture_ambient";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
