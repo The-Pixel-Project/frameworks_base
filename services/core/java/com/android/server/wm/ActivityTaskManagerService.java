818503e16a736e738c265b7f0d4cf4576a364103
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-08 16:55:44 +0530

    PixelPropsUtils: Un-spoof GMS while adding google account [2/2]
    GMS uses its DroidGuard service (the same that's used for safetynet)
    to get the device name from the build fingerprint, which from our
    current certified props, is Pixel XL. The device name after Google
    login shows up in the login alert accordingly as Pixel XL, which is
    inconvenient especially when you have multiple devices on the same ROM.
    
    By restarting the DroidGuard service process (gms.unstable) as soon as
    the user starts adding Google account (i.e when the GMS add account
    activity is started and moved to the top of the task stack), we can
    skip spoofing to certified props in this instance. When the user
    stops/finishes adding account (GMS add account activity is no longer
    at the top of the task stack), restart the process again, this time
    spoofing to certified props in order to be ready for safetynet.
    
    This ensures while adding a Google account, GMS reads the original
    properties of the device and labels the device with its original name,
    while in any other situation GMS reads certified properties required
    to pass SafetyNet/Play Integrity.
    
    Test:
    - Add google account from setup wizard, settings, any other app
    - Observe correct device name in gmail login alert
    - SafetyNet (basic, CTS), Play Integrity (basic, device) passes.
    
    Change-Id: Ia5463f220a8262f7963fc5b2640a7a5311a73210
    
    * Adapt to current project
    * Skip entire gms setProp process by return
    
    Original commit:
    https://github.com/ArrowOS/android_frameworks_base/commit/9f490f70b3fb11afa5df6adf7e99a225da563d5a
    
    Change-Id: I75b6a47bd382663b08fb1665329e49e913a7c091
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -288,6 +288,8 @@ import com.android.server.usage.AppStandbyInternal;
 
 import com.android.internal.util.custom.cutout.CutoutFullscreenController;
 
+import com.android.internal.util.custom.PixelPropsUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -1991,7 +1993,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        enforceTaskPermission("getFocusedRootTaskInfo()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("getFocusedRootTaskInfo()");
+        }
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -3110,7 +3114,9 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        enforceTaskPermission("registerTaskStackListener()");
+        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
+            enforceTaskPermission("registerTaskStackListener()");
+        }
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 
