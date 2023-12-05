d329f5c3782b19d897e0039cf6a56640033f298e
Pranav Vashi <neobuddy89@gmail.com>
2023-12-06 00:53:03 +0530

    VolumeDialogImpl: Attempt fixing initial dialog position
    Change-Id: Id902b34eb6c03f2d1044a8dab6ec91143fdfb675
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -418,6 +418,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
         if (mDevicePostureController != null) {
             mDevicePostureController.addCallback(mDevicePostureControllerCallback);
         }
+
+        mHandler.post(() -> {
+            mControllerCallbackH.onConfigurationChanged();
+        });
     }
 
     @Override
