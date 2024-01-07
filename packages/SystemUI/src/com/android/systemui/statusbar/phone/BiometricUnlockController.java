0a3b12b26535ecdac61f7c121dd18c6ea8b95d23
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-07 14:20:19 +0530

    SystemUI: Disable animations during biometric wake-and-unlock
    Animations slow down the phone. Gotta go fast especially while
    wake-and-unlocking with capacitive fingerprint scanners.
    Like @ jesec said in https://github.com/LineageOS/android_frameworks_base/commit/32244a3d0207df4bbd834fb64b3538752380dbb4,
    OEMs have been doing this for years.
    
    Based on:
    
    Author: Rishawn <70256146+Stealth1226@users.noreply.github.com>
    Date:   Mon Jan 10 09:47:15 2022 +0530
    
        SystemUI: Allow disabling ripple effect on unlock [1/2]
    
        neobuddy89:
        * Reverse logic. Enabled animation by default.
    
    Author: Nauval Rizky <enuma.alrizky@gmail.com>
    Date:   Wed Jan 19 08:31:56 2022 +0700
    
        SystemUI: Fix method for disabling unlock ripple animation
    
        NotificationShadeWindowController's are not gracefully closed, which
        leads to top parts of the screen is being obscured by invisible shade window.
        And also, "ripple" is actually still exists due to lightRevealScrim are
        being started by onKeyguardFadingAwayChanged, which is not what user expect
        when they want to disable the effect.
    
        So in order to handle those cases, lets intercept it from the
        AuthRippleController instead.
    
        [palaych] With system settings we need to pass UserHandle.USER_CURRENT to make sure we are getting proper value.
    
        Change-Id: I85ab96368c8b0099700f3de18ba95ce63eaac443
    
    Co-authored-by: Rishawn <70256146+Stealth1226@users.noreply.github.com>
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Change-Id: I12f152e2c62b22ec75bcf86591721de01ab39c70

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BiometricUnlockController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BiometricUnlockController.java
@@ -463,15 +463,15 @@ public class BiometricUnlockController extends KeyguardUpdateMonitorCallback imp
     public void startWakeAndUnlock(BiometricSourceType biometricSourceType,
                                    boolean isStrongBiometric) {
         int mode = calculateMode(biometricSourceType, isStrongBiometric);
+        startWakeAndUnlock(
+                mode,
+                BiometricUnlockSource.Companion.fromBiometricSourceType(biometricSourceType)
+        );
         if (mode == MODE_WAKE_AND_UNLOCK
                 || mode == MODE_WAKE_AND_UNLOCK_PULSING || mode == MODE_UNLOCK_COLLAPSING
                 || mode == MODE_WAKE_AND_UNLOCK_FROM_DREAM || mode == MODE_DISMISS_BOUNCER) {
             onBiometricUnlockedWithKeyguardDismissal(biometricSourceType);
         }
-        startWakeAndUnlock(
-                mode,
-                BiometricUnlockSource.Companion.fromBiometricSourceType(biometricSourceType)
-        );
     }
 
     /**
