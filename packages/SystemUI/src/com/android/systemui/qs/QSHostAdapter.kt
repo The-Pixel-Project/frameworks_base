7c29dfead0e7fcc95ba9a8ed0b0f9b0bcd45b16d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-03-11 19:40:18 +0200

    SystemUI: require unlocking to use QS tiles
    This reverts commit 37ea1b688d08dd28a483d2a5ae9548fa9e60c446 partially.
    
    Change-Id: Ib7f2e46fe1d3a6eb45b73c2c4471747546be0d7e

--- packages/SystemUI/src/com/android/systemui/qs/QSHostAdapter.kt
+++ packages/SystemUI/src/com/android/systemui/qs/QSHostAdapter.kt
@@ -31,6 +31,7 @@ import com.android.systemui.qs.pipeline.domain.interactor.CurrentTilesInteractor
 import com.android.systemui.qs.pipeline.shared.QSPipelineFlagsRepository
 import com.android.systemui.qs.pipeline.shared.TileSpec
 import com.android.systemui.shade.ShadeDisplayAware
+import com.android.systemui.statusbar.policy.KeyguardStateController
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
@@ -53,6 +54,7 @@ constructor(
     @ShadeDisplayAware private val context: Context,
     private val tileServiceRequestControllerBuilder: TileServiceRequestController.Builder,
     @Application private val scope: CoroutineScope,
+    private val keyguardStateController_: KeyguardStateController,
     dumpManager: DumpManager,
 ) : QSHost {
 
@@ -71,6 +73,8 @@ constructor(
     override fun getTiles(): Collection<QSTile> {
         return interactor.currentQSTiles
     }
+
+    override fun getKeyguardStateController() = keyguardStateController_
 
     override fun getSpecs(): List<String> {
         return interactor.currentTilesSpecs.map { it.spec }
