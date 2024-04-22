c0e5905387af6800f0a86f8389292cddcec957d2
Ido Ben-Hur <idoybh2@gmail.com>
2024-04-22 15:35:28 +0300

    base: Allow turning on Bluetooth when pressing the tile [1/2]
    Respectively
    Could save some taps for some users
    
    Change-Id: I1c3a2e9b37d7ea07d8d1ba2bee26998bd3c8ac02
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6642,7 +6642,13 @@ public final class Settings {
          */
         public static final String SMART_5G = "smart_5g";
 
-       /**
+        /**
+         * Whether to turn on Bluetooth automatically when showing the Bluetooth dialog
+         * @hide
+         */
+        public static final String QS_BT_AUTO_ON = "qs_bt_auto_on";
+
+        /**
          * These are all public system settings
          *
          * @hide
