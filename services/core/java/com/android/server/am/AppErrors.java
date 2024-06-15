3e8013b4745ccb53a3c3e06803ba78db389a2c9a
NurKeinNeid <mralexman3000@gmail.com>
2024-06-15 15:10:14 +0200

    core: Switch to mkrbin for copying crash logs
    Change-Id: I388a4b98e7a10ebba32cdbe4c98a62c340b902d1
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -655,7 +655,7 @@ class AppErrors {
                 return;
             }
 
-            // Add paste content for Memochō option
+            // Add paste content for MkrBin option
             data.paste = "time: " + timeMillis + "\n" +
                     "msg: " + longMsg + "\n" +
                     "stacktrace: " + stackTrace;
