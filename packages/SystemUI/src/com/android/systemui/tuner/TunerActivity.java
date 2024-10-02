a9c2d0dba89fc058d94e80a50ca2f847c9034371
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-02 21:32:58 +0530

    SystemUI: Fix insets for Status bar tuner
    neobuddy89:
    Pulled from - https://github.com/yaap/frameworks_base/commit/869cd1582aef516215f8bb0de720f0025bab67cb
    Rest stuff already added by LOS.
    
    Change-Id: Iaea806c441772e6de6289b079ddf0dc6f34487f3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
@@ -24,6 +24,9 @@ import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.graphics.Insets;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceScreen;
@@ -33,6 +36,8 @@ import com.android.systemui.demomode.DemoModeController;
 import com.android.systemui.fragments.FragmentService;
 import com.android.systemui.res.R;
 import com.android.systemui.util.settings.GlobalSettings;
+
+import com.google.android.material.color.DynamicColors;
 
 import javax.inject.Inject;
 
@@ -66,6 +71,22 @@ public class TunerActivity extends CollapsingToolbarBaseActivity implements
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         setContentView(R.layout.tuner_activity);
+
+        DynamicColors.applyToActivityIfAvailable(this);
+        setTheme(com.android.settingslib.widget.theme.R.style.Theme_SubSettingsBase);
+
+        // Handle window insets for padding adjustments
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.content_frame), (view, insets) -> {
+            Insets systemInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+            view.setPadding(
+                view.getPaddingLeft(),
+                view.getPaddingTop(),
+                view.getPaddingRight(),
+                systemInsets.bottom
+            );
+            return insets;
+        });
 
         if (getFragmentManager().findFragmentByTag(TAG_TUNER) == null) {
             final String action = getIntent().getAction();
