3f80f61c2935ff235bb2dd0f0d04b08d2388c740
Thomas Makin <halorocker89@gmail.com>
2024-11-23 17:53:45 +0000

    pm: aconfig: do not error on flags missing per partition
    Change-Id: Ifd9368b153197f2f5d9bf594cf56bb33413c0f19

--- core/java/com/android/internal/pm/pkg/component/AconfigFlags.java
+++ core/java/com/android/internal/pm/pkg/component/AconfigFlags.java
@@ -16,7 +16,6 @@
 
 package com.android.internal.pm.pkg.component;
 
-import static android.provider.flags.Flags.newStoragePublicApi;
 import static com.android.internal.pm.pkg.parsing.ParsingUtils.ANDROID_RES_NAMESPACE;
 
 import android.aconfig.DeviceProtos;
@@ -28,7 +27,6 @@ import android.annotation.Nullable;
 import android.content.res.Flags;
 import android.os.Environment;
 import android.os.Process;
-import android.os.flagging.AconfigPackage;
 import android.util.ArrayMap;
 import android.util.Slog;
 import android.util.Xml;
@@ -45,7 +43,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * A class that manages a cache of all device feature flags and their default + override values.
@@ -61,8 +58,7 @@ public class AconfigFlags {
     private static final String OVERRIDE_PREFIX = "device_config_overrides/";
     private static final String STAGED_PREFIX = "staged/";
 
-    private final Map<String, Boolean> mFlagValues = new ArrayMap<>();
-    private final Map<String, AconfigPackage> mAconfigPackages = new ConcurrentHashMap<>();
+    private final ArrayMap<String, Boolean> mFlagValues = new ArrayMap<>();
 
     public AconfigFlags() {
         if (!Flags.manifestFlagging()) {
@@ -71,35 +67,26 @@ public class AconfigFlags {
             }
             return;
         }
-
-        if (useNewStorage()) {
-            Slog.i(LOG_TAG, "Using new flag storage");
-        } else {
-            Slog.i(LOG_TAG, "Using OLD proto flag storage");
-            final var defaultFlagProtoFiles =
-                    (Process.myUid() == Process.SYSTEM_UID) ? DeviceProtos.parsedFlagsProtoPaths()
-                            : Arrays.asList(DeviceProtos.PATHS);
-            for (String fileName : defaultFlagProtoFiles) {
-                final File protoFile = new File(fileName);
-                if (!protoFile.isFile() || !protoFile.canRead()) {
-                    continue;
-                }
-                try (var inputStream = new FileInputStream(protoFile)) {
+        final var defaultFlagProtoFiles =
+                (Process.myUid() == Process.SYSTEM_UID) ? DeviceProtos.parsedFlagsProtoPaths()
+                        : Arrays.asList(DeviceProtos.PATHS);
+        for (String fileName : defaultFlagProtoFiles) {
+            File f = new File(fileName);
+            if (f.isFile() && f.canRead()) {
+                try (FileInputStream inputStream = new FileInputStream(fileName)) {
                     loadAconfigDefaultValues(inputStream.readAllBytes());
                 } catch (IOException e) {
-                    Slog.w(LOG_TAG, "Failed to read Aconfig values from " + fileName, e);
+                    Slog.e(LOG_TAG, "Failed to read Aconfig values from " + fileName, e);
                 }
-            }
-            if (Process.myUid() == Process.SYSTEM_UID) {
-                // Server overrides are only accessible to the system, no need to even try loading
-                // them in user processes.
-                loadServerOverrides();
+            } else {
+                Slog.w(LOG_TAG, "No Aconfig flags at " + fileName);
             }
         }
-    }
-
-    private static boolean useNewStorage() {
-        return newStoragePublicApi() && Flags.useNewAconfigStorage();
+        if (Process.myUid() == Process.SYSTEM_UID) {
+            // Server overrides are only accessible to the system, no need to even try loading them
+            // in user processes.
+            loadServerOverrides();
+        }
     }
 
     private void loadServerOverrides() {
@@ -124,9 +111,6 @@ public class AconfigFlags {
 
         final var settingsFile = new File(Environment.getUserSystemDirectory(0),
                 "settings_config.xml");
-        if (!settingsFile.isFile() || !settingsFile.canRead()) {
-            return;
-        }
         try (var inputStream = new FileInputStream(settingsFile)) {
             TypedXmlPullParser parser = Xml.resolvePullParser(inputStream);
             if (parser.next() != XmlPullParser.END_TAG && "settings".equals(parser.getName())) {
@@ -193,7 +177,7 @@ public class AconfigFlags {
                 }
             }
         } catch (IOException | XmlPullParserException e) {
-            Slog.w(LOG_TAG, "Failed to read Aconfig values from settings_config.xml", e);
+            Slog.e(LOG_TAG, "Failed to read Aconfig values from settings_config.xml", e);
         }
     }
 
@@ -221,60 +205,7 @@ public class AconfigFlags {
      */
     @Nullable
     public Boolean getFlagValue(@NonNull String flagPackageAndName) {
-        if (useNewStorage()) {
-            return getFlagValueFromNewStorage(flagPackageAndName);
-        } else {
-            Boolean value = mFlagValues.get(flagPackageAndName);
-            if (DEBUG) {
-                Slog.v(LOG_TAG, "Aconfig flag value for " + flagPackageAndName + " = " + value);
-            }
-            return value;
-        }
-    }
-
-    private Boolean getFlagValueFromNewStorage(String flagPackageAndName) {
-        // We still need to check mFlagValues in case addFlagValuesForTesting() was called for
-        // testing purposes.
-        if (!mFlagValues.isEmpty() && mFlagValues.containsKey(flagPackageAndName)) {
-            Boolean value = mFlagValues.get(flagPackageAndName);
-            if (DEBUG) {
-                Slog.v(
-                        LOG_TAG,
-                        "Aconfig flag value (FOR TESTING) for "
-                                + flagPackageAndName
-                                + " = "
-                                + value);
-            }
-            return value;
-        }
-
-        int index = flagPackageAndName.lastIndexOf('.');
-        if (index < 0) {
-            Slog.e(LOG_TAG, "Unable to parse package name from " + flagPackageAndName);
-            return null;
-        }
-        String flagPackage = flagPackageAndName.substring(0, index);
-        String flagName = flagPackageAndName.substring(index + 1);
-        Boolean value = null;
-        AconfigPackage aconfigPackage = mAconfigPackages.computeIfAbsent(flagPackage, p -> {
-            try {
-                return AconfigPackage.load(p);
-            } catch (Exception e) {
-                Slog.e(LOG_TAG, "Failed to load aconfig package " + p, e);
-                return null;
-            }
-        });
-        if (aconfigPackage != null) {
-            // Default value is false for when the flag is not found.
-            // Note: Unlike with the old storage, with AconfigPackage, we don't have a way to
-            // know if the flag is not found or if it's found but the value is false.
-            try {
-                value = aconfigPackage.getBooleanFlagValue(flagName, false);
-            } catch (Exception e) {
-                Slog.e(LOG_TAG, "Failed to read Aconfig flag value for " + flagPackageAndName, e);
-                return null;
-            }
-        }
+        Boolean value = mFlagValues.get(flagPackageAndName);
         if (DEBUG) {
             Slog.v(LOG_TAG, "Aconfig flag value for " + flagPackageAndName + " = " + value);
         }
