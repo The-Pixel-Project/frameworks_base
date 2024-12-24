b066bacf76eb34b3f95cc31e72f4b551ae7d83bb
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-12-24 17:32:28 +0530

    fixup! SystemUI: Constrain keyguard indication area burn-in offset
    Change-Id: I962981e1a90a09869d2150a086aa461688b994db

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardIndicationAreaBinder.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/KeyguardIndicationAreaBinder.kt
@@ -97,7 +97,7 @@ object KeyguardIndicationAreaBinder {
                     launch("$TAG#viewModel.indicationAreaTranslationY") {
                         configurationBasedDimensions
                             .map {
-                                maxOf(
+                                minOf(
                                     it.indicationAreaBottomMarginPx,
                                     it.defaultBurnInPreventionYOffsetPx
                                 )
