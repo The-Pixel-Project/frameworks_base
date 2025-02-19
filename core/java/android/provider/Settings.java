74d0e5876cf847d32abbad21318b1c5c6b4fbd00
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-02-19 15:08:51 +0530

    base: Add three-fingers-swipe to screenshot [1/2]
    The feature is ported from Oppo ColorOS.
    With the options on, users can swipe down
    on the screen to have a screenshot.
    Original smali port by: wuxianlin
    
    Change-Id: I87ed333f81e1259758ea899bf019061d8ab85fd0
    
    base: Improvements for swipe to screenshot
    
    * Cancel touch events when tap with three fingers
    
    * Dyrex2004: Fix typos (Screnshot -> Screenshot)
    
    Change-Id: I6e1228e4101aa357d1e26ca6d0db1c44428ef367
    
    base: SwipeToScreenshot: Import MIUI implementation
    
    Reverse engineered from Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/9.2.25:user/release-keys
    
    idoybh (YAAP):
    Improved code all across, remove the usage of a system prop and more...
    also properly add co-authors
    
    Co-Authored-By: Ido Ben-Hur <idoybh2@gmail.com>
    Co-Authored-By: wuxianlin <wuxianlinwxl@gmail.com>
    
    Change-Id: I1d5938457b7cc4c5ef1fec235d27d8a40526961a
    Co-Authored-By: Tobias Merkel <stylogey@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    
    Former-commit-id: 98e9a89b84291500772951f4282cd3d9f2618e89
    Change-Id: Ida9bccf101c3a07581e05a9fd9f93978ffb0fd4b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6609,6 +6609,21 @@ public final class Settings {
         public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
 
         /**
+         * Three Finger Gesture from Oppo
+         * @hide
+         */
+        @Readable
+        public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
+
+        /**
+         * Three Finger Gesture - flag for activation
+         * NOT for backup!
+         * @hide
+         */
+        @Readable
+        public static final String THREE_FINGER_GESTURE_ACTIVE = "three_finger_gesture_active";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -8133,6 +8133,11 @@ public final class ViewRootImpl implements ViewParent,
                 mLastClickToolType = event.getToolType(event.getActionIndex());
             }
 
+            if (event.getPointerCount() == 3 && isSwipeToScreenshotGestureActive()) {
+                event.setAction(MotionEvent.ACTION_CANCEL);
+                Log.d("SwipeToScreenShot", "canceling motionEvent because of threeGesture detecting");
+            }
+
             mAttachInfo.mUnbufferedDispatchRequested = false;
             mAttachInfo.mHandlingPointerEvent = true;
             // If the event was fully handled by the handwriting initiator, then don't dispatch it
@@ -13524,6 +13529,15 @@ public final class ViewRootImpl implements ViewParent,
         if (!sProtoLogInitialized) {
             ProtoLog.init(ViewProtoLogGroups.ALL_GROUPS);
             sProtoLogInitialized = true;
+        }
+    }
+
+    private boolean isSwipeToScreenshotGestureActive() {
+        try {
+            return ActivityManager.getService().isSwipeToScreenshotGestureActive();
+        } catch (RemoteException e) {
+            Log.e("SwipeToScreenshot", "isSwipeToScreenshotGestureActive exception", e);
+            return false;
         }
     }
 }
