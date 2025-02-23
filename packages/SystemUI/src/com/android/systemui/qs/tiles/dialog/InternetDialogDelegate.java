c0349e8d52f92210666af18c8e42585659227394
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-23 18:22:57 +0800

    SystemUI: Fix internet dialog background runtime exception
    Change-Id: I9ced4105db8393aea776dcee149e32ef7da1861f

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegate.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegate.java
@@ -162,6 +162,7 @@ public class InternetDialogDelegate implements
     protected Button mShareWifiButton;
     private Button mAirplaneModeButton;
     private Drawable mBackgroundOn;
+    private Drawable mSecondaryBackgroundOn;
     private final KeyguardStateController mKeyguard;
     @Nullable
     private Drawable mBackgroundOff = null;
@@ -321,6 +322,7 @@ public class InternetDialogDelegate implements
         mInternetDialogTitle.setText(getDialogTitleText());
         mInternetDialogTitle.setGravity(Gravity.START | Gravity.CENTER_VERTICAL);
         mBackgroundOff = context.getDrawable(R.drawable.internet_dialog_selected_effect);
+        mSecondaryBackgroundOn = mBackgroundOn.getConstantState().newDrawable().mutate();
         setOnClickListener(dialog);
         setHotspotLayout();
         mTurnWifiOnLayout.setBackground(null);
@@ -619,7 +621,7 @@ public class InternetDialogDelegate implements
                         R.id.secondary_mobile_network_layout);
                 mSecondaryMobileNetworkLayout.setOnClickListener(
                         this::onClickConnectedSecondarySub);
-                mSecondaryMobileNetworkLayout.setBackground(mBackgroundOn);
+                mSecondaryMobileNetworkLayout.setBackground(mSecondaryBackgroundOn);
 
                 TextView mSecondaryMobileTitleText = mDialogView.requireViewById(
                         R.id.secondary_mobile_title);
