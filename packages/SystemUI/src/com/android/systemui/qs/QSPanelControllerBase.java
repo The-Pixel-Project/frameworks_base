48ea89fbb8072065f06f2b4ef16eb5593ad9cedf
Dhina17 <dhinalogu@gmail.com>
2025-02-28 01:08:33 +0530

    FlashlightStrengthTile: TouchableQSTile -> SlideableQSTile
    Change-Id: I1d0e9c4db20f37b09828fd582dd6c289fefda6b6

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -42,7 +42,7 @@ import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tiles.FlashlightStrengthTile;
 import com.android.systemui.qs.tileimpl.QSTileViewImpl;
 import com.android.systemui.qs.tileimpl.SliderQSTileViewImpl;
-import com.android.systemui.qs.tileimpl.TouchableQSTile;
+import com.android.systemui.qs.tileimpl.SlideableQSTile;
 import com.android.systemui.scene.shared.flag.SceneContainerFlag;
 import com.android.systemui.shade.ShadeDisplayAware;
 import com.android.systemui.shade.shared.flag.ShadeWindowGoesAround;
@@ -391,13 +391,13 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         }
         final QSTileViewImpl tileView;
         if (FlashlightStrengthTile.TILE_SPEC.equals(tile.getTileSpec())) {
-            TouchableQSTile touchableTile = (TouchableQSTile) tile;
+            SlideableQSTile slideableTile = (SlideableQSTile) tile;
             tileView = new SliderQSTileViewImpl(
                     getContext(),
                     collapsedView,
-                    touchableTile.getTouchListener(),
-                    touchableTile.getSettingsSystemKey(),
-                    touchableTile.getSettingsDefaultValue());
+                    slideableTile.getTouchListener(),
+                    slideableTile.getSettingsSystemKey(),
+                    slideableTile.getSettingsDefaultValue());
         } else {
             tileView = new QSTileViewImpl(
                     getContext(), collapsedView, longPressEffect);
