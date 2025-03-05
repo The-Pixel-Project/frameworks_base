d35e3e46e3ecae16236b60a0fe3e4aa151d20e88
drkphnx <dark.phnx12@gmail.com>
2025-03-05 10:44:11 +0000

    base: Extend OngoingProgressbar feature to be controlled from Settings Get rid of the boolean config as well as we're going dynamic.
    Change-Id: I6ef2607bcd2b3364fd1c61a3d50b51478fe26558
    
    Co-authored-by: sajidshahriar72543 <sazidshahriar39@gmail.com>
    Change-Id: I86acee6567cc2f10e9df65dc727199901fcc1a58
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6916,6 +6916,12 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Whether to show ongoing progressbar in the notification shade
+         * @hide
+         */
+        public static final String CONFIG_ONGOING_PROGRESSBAR = "config_enableOngoingProgressBarChip";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
