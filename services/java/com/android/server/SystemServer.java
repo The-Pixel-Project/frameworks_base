b5e1cbe296dbb225634896c04cfa5796bfe3fc73
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-02 15:50:55 +0800

    services: Add GameSpaceManagerService
    automatically add/remove games to gamespace
    
    Squashed:
    
        From: rmp22 <195054967+rmp22@users.noreply.github.com>
        Date: Thu, 17 Apr 2025 08:12:06 +0800
        Subject: [PATCH] GameSpaceManagerService: sanitize game list
    
        fixes malformed settings value:
        com.kurogame.wutheringwaves.global=2;com.mobile.legends=2;com.glohow.global.blackbeacon=2=1=1=1=1=1=1=1;com.mobile.legends=2;
    
        Change-Id: I7a905b54d603bee111032185a720044697764cc2
        Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    
    Change-Id: I73c89e2ac1a078d540718d96437f1a66b9173587
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2799,6 +2799,10 @@ public final class SystemServer implements Dumpable {
             t.traceBegin("AttestationService");
             mSystemServiceManager.startService(AttestationService.class);
             t.traceEnd();
+
+            t.traceBegin("GameSpaceManagerService");
+            mSystemServiceManager.startService(GameSpaceManagerService.class);
+            t.traceEnd();
         }
 
         t.traceBegin("StartMediaProjectionManager");
