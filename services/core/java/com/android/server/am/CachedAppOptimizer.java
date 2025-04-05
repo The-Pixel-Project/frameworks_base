d91281e752680cc426ca7b1a9bf600e9cb1f62ac
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-05 09:41:36 +0800

    services: Do not freeze foreground processes
    freezing fg procs results to exception when restarting killed fg procs. e.g launcher is restarted by system while frozen
    
    Change-Id: Id935dbb6ed50bf115f389f2423f161479ebe9c6b
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/am/CachedAppOptimizer.java
+++ services/core/java/com/android/server/am/CachedAppOptimizer.java
@@ -1152,6 +1152,9 @@ public class CachedAppOptimizer {
     @GuardedBy({"mAm", "mProcLock"})
     private void freezeAppAsyncInternalLSP(ProcessRecord app, @UptimeMillisLong long delayMillis,
             boolean force) {
+        if (app.mState.hasForegroundActivities() || app.mState.hasOverlayUi()) {
+            return;
+        }
         final ProcessCachedOptimizerRecord opt = app.mOptRecord;
         if (opt.isPendingFreeze()) {
             if (delayMillis == 0) {
