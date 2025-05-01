caf95c183bfb6a4a2c054a7d92d4eca1b020b188
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-01 08:33:48 +0800

    SystemServer: release memory after clearing garbage fds
    Change-Id: I38ecb1b5257a5ce79cda335838bb3a05af94fe64
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -641,6 +641,7 @@ public final class SystemServer implements Dumpable {
                     System.gc();
                     System.runFinalization();
                     System.gc();
+                    releaseMemory();
                     maxFd = getMaxFd();
                 }
             } catch (Exception e) {
@@ -649,6 +650,13 @@ public final class SystemServer implements Dumpable {
         };
 
         scheduler.scheduleWithFixedDelay(fdCleanerTask, 0, checkInterval, TimeUnit.SECONDS);
+    }
+
+    private static void releaseMemory() {
+        try {
+            android.app.ActivityManager.getService().releaseMemory(900, 20, false, false);
+        } catch (Exception e) {
+        }
     }
 
     /**
