AxionAOSP/android_frameworks_base

base: Add support for hide applist [1/2] * Based on commit "Hide hidden apps from all apps except system" * Hide user selected apps from applist * The applist includes system apps (including overlays), and user apps
Ref:
https://review.calyxos.org/c/CalyxOS/platform_frameworks_base/+/16571
TheParasiteProject/frameworks_base@6c41a0f
https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
yaap/frameworks_base@232260f

Change-Id: If6c94fd345c488edab11927c4f567bc2171f69b2
Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>
Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

core/java/com/android/internal/util/axion/HideAppListUtils.java
@@ -1,0 +1,115 @@
+package com.android.internal.util.axion;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.SystemProperties;
+import android.provider.Settings;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class HideAppListUtils {
+    enum Action {
+        ADD,
+        REMOVE,
+        SET
+    }
+
+    private static boolean isBootCompleted() {
+        return SystemProperties.getBoolean("sys.boot_completed", false);
+    }
+
+    public static boolean shouldHideAppList(Context context, String packageName) {
+        return shouldHideAppList(context.getContentResolver(), packageName);
+    }
+
+    public static boolean shouldHideAppList(ContentResolver cr, String packageName) {
+        if (cr == null || packageName == null || !isBootCompleted()) {
+            return false;
+        }
+
+        Set<String> apps = getApps(cr);
+        if (apps.isEmpty()) {
+            return false;
+        }
+
+        return apps.contains(packageName);
+    }
+
+    public static Set<String> getApps(Context context) {
+        if (context == null) {
+            return new HashSet<>();
+        }
+
+        return getApps(context.getContentResolver());
+    }
+
+    public static Set<String> getApps(ContentResolver cr) {
+        if (cr == null) {
+            return new HashSet<>();
+        }
+
+        String apps = "";
+        try {
+            apps = Settings.Secure.getString(cr, Settings.Secure.HIDE_APPLIST);
+        } catch (IllegalStateException e) {
+            return new HashSet<>();
+        }
+        if (apps != null && !apps.isEmpty() && !apps.equals(",")) {
+            return new HashSet<>(Arrays.asList(apps.split(",")));
+        }
+
+        return new HashSet<>();
+    }
+
+    private static void putAppsForUser(
+            Context context, String packageName, int userId, Action action) {
+        if (context == null || userId < 0) {
+            return;
+        }
+
+        final Set<String> apps = getApps(context);
+        switch (action) {
+            case ADD:
+                apps.add(packageName);
+                break;
+            case REMOVE:
+                apps.remove(packageName);
+                break;
+            case SET:
+                // Don't change
+                break;
+        }
+
+        Settings.Secure.putStringForUser(
+                context.getContentResolver(),
+                Settings.Secure.HIDE_APPLIST,
+                String.join(",", apps),
+                userId);
+    }
+
+    public void addApp(Context mContext, String packageName, int userId) {
+        if (mContext == null || packageName == null || userId < 0) {
+            return;
+        }
+
+        putAppsForUser(mContext, packageName, userId, Action.ADD);
+    }
+
+    public void removeApp(Context mContext, String packageName, int userId) {
+        if (mContext == null || packageName == null || userId < 0) {
+            return;
+        }
+
+        putAppsForUser(mContext, packageName, userId, Action.REMOVE);
+    }
+
+    public void setApps(Context mContext, int userId) {
+        if (mContext == null || userId < 0) {
+            return;
+        }
+
+        putAppsForUser(mContext, null, userId, Action.SET);
+    }
+}

services/core/java/com/android/server/pm/ComputerEngine.java
@@ -2612,6 +2612,11 @@ public class ComputerEngine implements Computer {
         if (ps.getUserStateOrDefault(userId).isHidden()) {
             return true;
         }
+        // if the target is included in Settings.Secure.HIDE_APPLIST, do filter
+        if (com.android.internal.util.axion.HideAppListUtils.shouldHideAppList(
+                mContext, packageName)) {
+            return true;
+        }
 
         return false;
     }

The-Pixel-Project/frameworks_base

core/java/com/android/internal/util/custom/HideAppListUtils.java
@@ -1,0 +1,115 @@
+package com.android.internal.util.custom;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.SystemProperties;
+import android.provider.Settings;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class HideAppListUtils {
+    enum Action {
+        ADD,
+        REMOVE,
+        SET
+    }
+
+    private static boolean isBootCompleted() {
+        return SystemProperties.getBoolean("sys.boot_completed", false);
+    }
+
+    public static boolean shouldHideAppList(Context context, String packageName) {
+        return shouldHideAppList(context.getContentResolver(), packageName);
+    }
+
+    public static boolean shouldHideAppList(ContentResolver cr, String packageName) {
+        if (cr == null || packageName == null || !isBootCompleted()) {
+            return false;
+        }
+
+        Set<String> apps = getApps(cr);
+        if (apps.isEmpty()) {
+            return false;
+        }
+
+        return apps.contains(packageName);
+    }
+
+    public static Set<String> getApps(Context context) {
+        if (context == null) {
+            return new HashSet<>();
+        }
+
+        return getApps(context.getContentResolver());
+    }
+
+    public static Set<String> getApps(ContentResolver cr) {
+        if (cr == null) {
+            return new HashSet<>();
+        }
+
+        String apps = "";
+        try {
+            apps = Settings.Secure.getString(cr, Settings.Secure.HIDE_APPLIST);
+        } catch (IllegalStateException e) {
+            return new HashSet<>();
+        }
+        if (apps != null && !apps.isEmpty() && !apps.equals(",")) {
+            return new HashSet<>(Arrays.asList(apps.split(",")));
+        }
+
+        return new HashSet<>();
+    }
+
+    private static void putAppsForUser(
+            Context context, String packageName, int userId, Action action) {
+        if (context == null || userId < 0) {
+            return;
+        }
+
+        final Set<String> apps = getApps(context);
+        switch (action) {
+            case ADD:
+                apps.add(packageName);
+                break;
+            case REMOVE:
+                apps.remove(packageName);
+                break;
+            case SET:
+                // Don't change
+                break;
+        }
+
+        Settings.Secure.putStringForUser(
+                context.getContentResolver(),
+                Settings.Secure.HIDE_APPLIST,
+                String.join(",", apps),
+                userId);
+    }
+
+    public void addApp(Context mContext, String packageName, int userId) {
+        if (mContext == null || packageName == null || userId < 0) {
+            return;
+        }
+
+        putAppsForUser(mContext, packageName, userId, Action.ADD);
+    }
+
+    public void removeApp(Context mContext, String packageName, int userId) {
+        if (mContext == null || packageName == null || userId < 0) {
+            return;
+        }
+
+        putAppsForUser(mContext, packageName, userId, Action.REMOVE);
+    }
+
+    public void setApps(Context mContext, int userId) {
+        if (mContext == null || userId < 0) {
+            return;
+        }
+
+        putAppsForUser(mContext, null, userId, Action.SET);
+    }
+}

services/core/java/com/android/server/pm/ComputerEngine.java
@@ -2553,6 +2553,11 @@ public class ComputerEngine implements Computer {
         if (ps.getUserStateOrDefault(userId).isHidden()) {
             return true;
         }
+        // if the target is included in Settings.Secure.HIDE_APPLIST, do filter
+        if (com.android.internal.util.custom.HideAppListUtils.shouldHideAppList(
+                mContext, packageName)) {
+            return true;
+        }
 
         return false;
     }